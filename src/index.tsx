82a5d01bd6e0443f4fba9b4df8e401989831817c
y-mgw <gomamikan@gmail.com>
2020-04-12 10:09:25 +0900

    router

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+import routes from './routes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const history = createBrowserHistory();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('root'));
+
 serviceWorker.unregister();
